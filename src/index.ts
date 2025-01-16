092a6fae90e48687a03fae89b0aae5b621a5868a
Baruch Ginzburg <W050459990@gmail.com>
2025-01-16 18:58:48 +0200

    create db connect

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import express, { urlencoded, json } from "express";
-import exampleRouter from "./routes/event_route.js"
 import 'dotenv/config';
+import connectDB from "./db.js";
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import swaggerOptions from "./swaggerOptions.ts";
+import eventRouter from "./routes/eventRouter.ts";
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(urlencoded({ extended: true }));
+
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
 app.use(json());
 
-app.use('/events', exampleRouter);
+
+// Connect to MongoDB
+connectDB();
+
+// Routers
+app.use('/events', eventRouter);
 
 app.listen(port, () => {
   console.log(`Sportify server is listening at port ${port}`);
